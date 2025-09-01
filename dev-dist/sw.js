339de324e267061bb28a3385c24d040ad90f21ff
Arnab Roy <mr.roy.arnab@gmail.com>
2025-09-01 14:29:14 +0530

    feat: ourcamu logo

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e7681877'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ak2b1mo7i28"
+    "revision": "0.77kt4nv45r8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
